c94867a3130d2c14b049e7b936c74e1bba6cc87a
saranjith <saranjith.s@attinadsoftware.com>
2019-12-04 16:23:08 +0530

    initial commit

---
+++ src/in/saran/Main.java
@@ -0,0 +1,50 @@
+/**
+ * @author: saranjith
+ * @date: 04-12-2019
+ */
+
+package in.saran;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int rangeStart = 134564;
+        int rangeEnd = 585159;
+        int count = 0;
+
+
+        for(int i=rangeStart;i<=rangeEnd;i++){
+            if(isInRightOrder(i)){
+
+                for(int j=0;j<Integer.toString(i).length()-1;j++) {
+                    if (Character.toString(Integer.toString(i).charAt(j)).equals(Character.toString(Integer.toString(i).charAt(j + 1)))) {
+                        System.out.println(i);
+                        count++;
+                        j = Integer.toString(i).length() - 1;
+                    }
+
+                }
+
+                }
+        }
+        System.out.println("testing isTidy "+count);
+    }
+
+    static boolean isInRightOrder(int number)
+    {
+        int c = 10;
+        while (number!=0)
+        {
+            int rem = number % 10;
+            number /= 10;
+            if (rem > c)
+                return false;
+            c = rem;
+        }
+
+        return true;
+    }
+
+}
+
